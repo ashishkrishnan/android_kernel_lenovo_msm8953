eff90f5aceb8ed0145467247d7b3435d12ab29fa
Subhaam <derksoul98@gmail.com>
2017-12-20 14:10:16 +0000

    update defconfig
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ VERSION="v1"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="kuntao"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DEVICE-$DATE.zip
-defconfig=lineageos_kuntao_defconfig
+defconfig=p2a42_defconfig
 THREAD="$(nproc --all)"
 
 # Dirs
